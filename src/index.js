d65475a40793b4aafe75b11ba2d14cc02a9d53f5
Darshan Hiragar <darshanhiragar44@gmail.com>
2024-08-07 10:28:22 +0530

    link issues

--- src/index.js
+++ src/index.js
@@ -25,11 +25,11 @@ root.render(
         <Route path="" element={<Home/>}/>
         <Route  path="/" element={<Home/>}/>
         <Route  path="/Portfolio" element={<Home/>}/>
-        <Route  path="/Home" element={<Home/>}/>
-        <Route path='/Projects' element={<Projects/>}/> 
-        <Route path='/Contact' element={<Contact/>}/> 
-        <Route path='/Skills' element={<Skills/>}/> 
-        <Route path='/Follow' element={<Social/>}/> 
+        <Route  path="/Portfolio/Home" element={<Home/>}/>
+        <Route path='/Portfolio/Projects' element={<Projects/>}/> 
+        <Route path='/Portfolio/Contact' element={<Contact/>}/> 
+        <Route path='/Portfolio/Skills' element={<Skills/>}/> 
+        <Route path='/Portfolio/Follow' element={<Social/>}/> 
     </Routes>
 </BrowserRouter>
     <Footer/>
