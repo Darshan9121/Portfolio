3e8d39259caa0b5f189f1d1e94a6704193aaff72
Darshan Hiragar <darshanhiragar44@gmail.com>
2024-08-12 21:04:35 +0530

    c

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,12 +8,12 @@ const Navbar = () => {
        <a href="./" className='text-thapa-400 text-xl'>Darshan Hiragar</a>
       </div>
        <ul className='md:flex md:gap-3 hidden md:block '>
-        <li><a href="http://darshan9121.github.io/Portfolio/">Home</a></li>
+        <li><a href="Portfolio/">Home</a></li>
         <li><a href="">Home</a></li>
         {/* <li><a href="http://">About</a></li> */}
-        <li><a href="https://darshan9121.github.io/Portfolio/skills">Skills</a></li>
-        <li><a href="https://darshan9121.github.io/Portfolio/Projects">Projects</a></li>
-        <li><a href="https://darshan9121.github.io/Portfolio/Portfolio/Follow">Follow Me</a></li>
+        <li><a href="Portfolio/skills">Skills</a></li>
+        <li><a href="Portfolio/Projects">Projects</a></li>
+        <li><a href="Portfolio/Follow">Follow Me</a></li>
        </ul>
        <button className='bg-thapa-400 text-black p-3 rounded-md'><a href='/contact'>Say Hello</a></button>
      </nav> 
